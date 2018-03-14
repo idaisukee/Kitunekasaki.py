04f1fea0a5a9d55582eab0f0c3d51bbd7aea58ae
Daisuke Inoue <hibariyashiki@gmail.com>
2018-03-15 01:31:09 +0900

    config1

---
+++ config1.py
@@ -0,0 +1,13 @@
+import re
+from xkeysnail.transform import *
+
+define_modmap({
+    Key.GRAVE: Key.ESC,
+    Key.TAB: Key.LEFT_ALT,
+    Key.CAPSLOCK: Key.LEFT_CTRL,
+    Key.RO: Key.RIGHT_SHIFT,
+    Key.BACKSLASH: Key.RIGHT_CTRL,
+    Key.ENTER: Key.RIGHT_ALT,
+    Key.INSERT: Key.BACKSPACE,
+    Key.F: Key.U
+})
