e0e26a71c28cf172947d39dceca1730b3e10050f
Daisuke Inoue <hibariyashiki@gmail.com>
2018-03-15 12:42:29 +0900

    config1.py

--- config1.py
+++ config1.py
@@ -9,5 +9,12 @@ define_modmap({
     Key.BACKSLASH: Key.RIGHT_CTRL,
     Key.ENTER: Key.RIGHT_ALT,
     Key.INSERT: Key.BACKSPACE,
-    Key.F: Key.U
+
+    Key.RIGHT_BRACE: Key.EQUAL,
+    Key.PAUSE: Key.EQUAL,
+
+    Key.R: Key.P,
+    Key.T: Key.Y,
+    Key.Y: Key.F,
+    Key.U: Key.G,
 })
